2a2dd9305931aadd71a4b73fbee2453a9f6901ad
HoJin9622 <kiss0104040@gmail.com>
2022-10-11 00:29:15 +0900

    feat: 채용 공고 등록 API

--- recruits/serializers.py
+++ recruits/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
+from companies.models import Company
 from .models import Recruit
 
 
 class RecruitSerializer(serializers.ModelSerializer):
+    company_id = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = Recruit
         exclude = ()
+        read_only_fields = ("company",)
+
+    def validate_company_id(self, value):
+        try:
+            Company.objects.get(pk=value)
+            return value
+        except Company.DoesNotExist:
+            raise serializers.ValidationError("Not exists company")
+
+    def create(self, validated_data):
+        recruit = Recruit.objects.create(**validated_data)
+        return recruit
