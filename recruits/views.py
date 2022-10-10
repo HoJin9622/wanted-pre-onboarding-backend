2a103347ce972b972f3519d058ce387ded20351b
HoJin9622 <kiss0104040@gmail.com>
2022-10-11 01:07:34 +0900

    feat: 채용 검색

--- recruits/views.py
+++ recruits/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from .models import Recruit
 from .serializers import RecruitSerializer
@@ -7,3 +8,15 @@ class RecruitViewSet(ModelViewSet):
 
     queryset = Recruit.objects.all()
     serializer_class = RecruitSerializer
+
+    def get_queryset(self):
+        queryset = Recruit.objects.all()
+        search = self.request.query_params.get("search")
+        if search is not None:
+            queryset = queryset.filter(
+                Q(position__icontains=search)
+                | Q(description__icontains=search)
+                | Q(skill__icontains=search)
+                | Q(company__name__icontains=search)
+            )
+        return queryset
