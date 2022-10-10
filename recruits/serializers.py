12f405cfe341c6064c32e33f2864cbaf65e66de7
HoJin9622 <kiss0104040@gmail.com>
2022-10-11 00:54:28 +0900

    feat: 채용 공고 목록

--- recruits/serializers.py
+++ recruits/serializers.py
@@ -5,11 +5,14 @@ from .models import Recruit
 
 class RecruitSerializer(serializers.ModelSerializer):
     company_id = serializers.IntegerField(write_only=True)
+    company_name = serializers.SerializerMethodField(read_only=True)
+    company_nation = serializers.SerializerMethodField(read_only=True)
+    company_area = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Recruit
-        exclude = ()
-        read_only_fields = ("company",)
+        exclude = ("company",)
+        read_only_fields = ()
 
     def validate_company_id(self, value):
         request = self.context.get("request")
@@ -24,3 +27,12 @@ class RecruitSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         recruit = Recruit.objects.create(**validated_data)
         return recruit
+
+    def get_company_name(self, obj):
+        return obj.company.name
+
+    def get_company_nation(self, obj):
+        return obj.company.nation
+
+    def get_company_area(self, obj):
+        return obj.company.area
